219556fe0f3d06775b344ebeb9a2c97e2c05c0a9
Maxim Brajsky <kzerot@gmail.com>
2018-12-03 22:51:57 +0300

    jjj

--- godot-bot.py
+++ godot-bot.py
@@ -1,7 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.error import (TelegramError, Unauthorized, BadRequest, 
+                            TimedOut, ChatMigrated, NetworkError)
 
-
-updater = Updater(token='732239525:AAGWemSOEAfW3B6bF4m3cyesDUAtRMHDQL')
+updater = Updater(token='732239525:AAGWemSOEAfW3B6bF4m3cyesDUAtRMHDQL4')
 dispatcher = updater.dispatcher
 
 # Обработка команд
@@ -14,13 +15,33 @@ def textMessage(bot, update):
     response = 'Получил Ваше сообщение: ' + update.message.text
     bot.send_message(chat_id=update.message.chat_id, text=response)
     
-    
+
+
+
+def error_callback(bot, update, error):
+    try:
+        raise error
+    except Unauthorized:
+        print("Error Unauthorized")
+    except BadRequest:
+        print("Error BadRequest")
+    except TimedOut:
+        print("Error TimedOut")
+    except NetworkError:
+        print("Error NetworkError")
+    except ChatMigrated as e:
+        print("Error ChatMigrated")
+    except TelegramError:
+        print("Error TelegramError")
+
+
 # Хендлеры
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
 # Добавляем хендлеры в диспетчер
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
+dispatcher.add_error_handler(error_callback)
 # Начинаем поиск обновлений
 updater.start_polling(clean=True)
 # Останавливаем бота, если были нажаты Ctrl + C
